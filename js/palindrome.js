b9f6aafa7ef45cffef68b8a2fece1a5ab94422fe
FrancescoCina <francesco.cina04@gmail.com>
2021-07-12 17:00:31 +0200

    agganciato script palindrome.js e fatti passaggi in italiano

---
+++ js/palindrome.js
@@ -0,0 +1,21 @@
+/*
+Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+/*
+1-prompt per richiedere all'utente la parola;
+2- funzione per controllare se la parola è palindroma: 
+2a- trasformo string in array(split);
+2b- applico metodo reverse all'array;
+2c- se array "reversato" === a userWord, allora valore true;
+2d- se array "reversato" !== a userWord, allora valore false;
+3- se valore true, la parola è palindroma;
+4- se valore false, la parola NON è palindroma;
+*/
+
+
+
+console.log("JS OK");
+
